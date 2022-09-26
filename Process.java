2a0d07185b024fba98056189459f39fe22bb665c
sujk912 <aa991122aa@gmail.com>
2022-09-26 16:53:26 +0900

    week4-1/lect

--- Process.java
+++ Process.java
@@ -1,6 +1,46 @@
 import java.util.Vector;
 
 public class Process {
+    private class ProcessControlBlock {
+        private int pid; // 프로세스 id
+
+        // Account
+        private int oid; // owner Id
+
+        // Statistics
+        // io Status Information (IO가 끝났는지 아닌지, 어떤 IO가 끝났는지, 어떤 IO가 시작되었는지 등)
+        private int ioDeviceId;
+        private int ioDeviceStatus;
+        public enum Estatus{
+            eReady,
+            eRunning,
+            eWaiting,
+            eSuspended, // 일시중단됨.
+        }
+        private Estatus status;
+
+        private class CPUContext { // TODO: CPU와 Process가 공유해서 쓸 거임.
+            // Segment Table (about segment registers)
+            private int cs;
+            private int ds;
+            private int ss;
+            private int hs;
+
+            // cu
+            private int PC;
+
+            // alu
+            private int AC;
+
+            // memory interface
+            private int MAR;
+            private int MBR;
+        }
+        private CPUContext cpuContext;
+
+
+
+    }
     private int PC;
 
     private Vector<Instruction> instructions;
@@ -8,49 +48,16 @@ public class Process {
 
     public Process(){
         this.instructions = new Vector<Instruction>();
-        this.instructions.add(new L1());
-        this.instructions.add(new L2());
-        this.instructions.add(new L3());
-        this.instructions.add(new L4());
-        this.instructions.add(new L5());
     }
+
     public void execute (){
-        instructions.get(PC).execute();
+        instructions.get(PC).execute("");
         PC++;
     }
 
-    private abstract class Instruction {
-        abstract int execute();
-    }
-
-    private class L1 extends Instruction {
-        int execute(){
-            System.out.println(this.getClass().getName());
-            return 0;
-        }
-    }
-    private class L2 extends Instruction {
-        int execute(){
-            System.out.println(this.getClass().getName());
-            return 0;
-        }
-    }
-    private class L3 extends Instruction {
-        int execute(){
-            System.out.println(this.getClass().getName());
-            return 0;
-        }
-    }
-    private class L4 extends Instruction {
-        int execute(){
-            System.out.println(this.getClass().getName());
-            return 0;
-        }
-    }
-    private class L5 extends Instruction {
-        int execute(){
-            System.out.println(this.getClass().getName());
-            return 0;
+    private class Instruction {
+        public void execute(String instruction){
+            System.out.println("Instruction: " + instruction);
         }
     }
 }
