c667e792b25e54da30bdd4043c8b5c78e95f9ff6
sujk912 <aa991122aa@gmail.com>
2022-10-03 15:54:00 +0900

    Upgrade Loader can read assembly language

--- Loader.java
+++ Loader.java
@@ -1,6 +1,58 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Loader {
     // HDD exe를 읽어서 exe파일의 메모리 주소를 Scheduler에게 주는 역할을 수행.
-    public Process load(String processName){
-        return new Process();
+//    .program
+//            .data
+//    codeSize 40
+//    dataSize 4096
+//    stackSize 4096
+//    heapSize 4096
+//            .code
+//    move @0, 0
+//    interrupt read
+//    move @4, r0
+//    move @8, 0
+//            .label loop
+//    compare @0, @4
+//    jumpGraterThan loopEnd
+//    add @8, 1
+//    jump label
+//.label loopEnd
+//    move r0, @8
+//    interrupt write
+//    halt
+//            .end
+
+    public void load(String exeName){
+        try {
+            File file = new File("Data"+"/"+exeName);
+            Scanner sc = new Scanner(file);
+            this.parse(sc);
+            sc.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void parse(Scanner sc){
+        String command = sc.next();
+        command = sc.next();
+        parseData(sc);
+        parseCode(sc);
+    }
+    private void parseData(Scanner sc){
+        String command = sc.next();
+        while (command.compareTo(".end") != 0) {
+//            parseData(sc);
+        }
+    }
+
+    private void parseCode(Scanner sc) {
+        String command = sc.next();
+        while (command.compareTo(".end") != 0) {
+//            parseCode(sc);
+        }
     }
 }
---
+++ UI.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UI {
+    public void run() {
+        Loader loader = new Loader();
+        Scheduler scheduler = new Scheduler();
+        // console command
+        // "r fileName -> execute fileName"
+        // "q -> quit program"
+        Scanner sc = new Scanner(System.in);
+        String command = sc.next();
+        while(command.compareTo("q") != 0){
+            if(command.compareTo("r") == 0){
+                String fileName = sc.next();
+                loader.load(fileName);
+            }
+            scheduler.run();
+            command = sc.next();
+        }
+
+        sc.close();
+    }
+}
